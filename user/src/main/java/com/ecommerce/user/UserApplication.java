00a3055d01775a0da5c80577082ab86315f6d5db
hoanghaidang <hoanghaidang@luvina.net>
2026-02-09 18:41:32 +0700

    user-service: integrate config server and environment loader

--- user/src/main/java/com/ecommerce/user/UserApplication.java
+++ user/src/main/java/com/ecommerce/user/UserApplication.java
@@ -1,13 +1,17 @@
 package com.ecommerce.user;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import com.ecommerce.user.env.EnvLoader;
 
 @SpringBootApplication
 public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		new SpringApplicationBuilder(UserApplication.class)
+			.initializers(new EnvLoader())
+			.run(args);
 	}
 
 }
---
+++ user/src/main/java/com/ecommerce/user/env/EnvLoader.java
@@ -0,0 +1,22 @@
+package com.ecommerce.user.env;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class EnvLoader
+        implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext context) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory("user")
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
+        System.out.println("ENV loaded from config/.env");
+    }
+}
