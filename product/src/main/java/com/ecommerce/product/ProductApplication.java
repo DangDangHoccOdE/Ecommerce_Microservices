ccaf9801a6a2dfbb7a2ff29c390b1f824fa3fde9
hoanghaidang <hoanghaidang@luvina.net>
2026-02-09 18:41:27 +0700

    product-service: add config client setup and message endpoint

--- product/src/main/java/com/ecommerce/product/ProductApplication.java
+++ product/src/main/java/com/ecommerce/product/ProductApplication.java
@@ -2,15 +2,19 @@ package com.ecommerce.product;
 
 import java.util.TimeZone;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import com.ecommerce.product.env.EnvLoader;
 
 @SpringBootApplication
 public class ProductApplication {
 
 	public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
-		SpringApplication.run(ProductApplication.class, args);
+		new SpringApplicationBuilder(ProductApplication.class)
+			.initializers(new EnvLoader())
+			.run(args);
 	}
 
 }
