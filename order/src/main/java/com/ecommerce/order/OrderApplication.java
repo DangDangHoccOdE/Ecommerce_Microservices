47e88de269d7da53fb6424b1fd4445bd108197b0
hoanghaidang <hoanghaidang@luvina.net>
2026-02-09 18:41:21 +0700

    order-service: add config client setup and message endpoint

--- order/src/main/java/com/ecommerce/order/OrderApplication.java
+++ order/src/main/java/com/ecommerce/order/OrderApplication.java
@@ -2,15 +2,19 @@ package com.ecommerce.order;
 
 import java.util.TimeZone;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import com.ecommerce.order.env.EnvLoader;
 
 @SpringBootApplication
 public class OrderApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
-		SpringApplication.run(OrderApplication.class, args);
+		new SpringApplicationBuilder(OrderApplication.class)
+			.initializers(new EnvLoader())
+			.run(args);
 	}
 
 }
