ee1598912bce47f712674b42d0309036f661a123
DangDangHocCode <haidang972004@gmail.com>
2026-02-22 21:50:47 +0700

    feat(gateway): add circuit breaker and fallback handler

--- gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class GatewayConfig {
@@ -11,22 +12,27 @@ public class GatewayConfig {
     public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("product-service", r -> r.path("/api/products/**")
-                    //    .filters(f -> f.rewritePath("/products(?<segment>/?.*)",
-                    //            "/api/products${segment}"))                       
-                        .uri("lb://PRODUCT-SERVICE"))                     
+                        .filters(f -> f
+                                .retry(retryConfig -> retryConfig.setRetries(10)
+                                        .setMethods(HttpMethod.GET))
+                                .circuitBreaker(config -> config.setName("ecomBreaker")
+                                        .setFallbackUri("forward:/fallback/products")))
+                        // .filters(f -> f.rewritePath("/products(?<segment>/?.*)",
+                        // "/api/products${segment}"))
+                        .uri("lb://PRODUCT-SERVICE"))
                 .route("user-service", r -> r.path("/api/users/**")
-                    //    .filters(f -> f.rewritePath("/users(?<segment>/?.*)",
-                    //            "/api/users${segment}"))                
+                        // .filters(f -> f.rewritePath("/users(?<segment>/?.*)",
+                        // "/api/users${segment}"))
                         .uri("lb://USER-SERVICE"))
-                .route("order-service", r -> r.path("/api/orders/**, /api/cart/**")
-                    //    .filters(f -> f.rewritePath("/(?<segment>.*)",
-                    //            "/api/${segment}"))                
+                .route("order-service", r -> r.path("/api/orders/**", "/api/cart/**")
+                        // .filters(f -> f.rewritePath("/(?<segment>.*)",
+                        // "/api/${segment}"))
                         .uri("lb://ORDER-SERVICE"))
                 .route("eureka-server", r -> r.path("/eureka/main")
                         .filters(f -> f.rewritePath("/eureka/main", "/"))
-                        .uri("http://localhost:8761"))                                                                   
+                        .uri("http://localhost:8761"))
                 .route("eureka-server-static", r -> r.path("/eureka/**")
-                        .uri("http://localhost:8761"))                        
+                        .uri("http://localhost:8761"))
                 .build();
     }
 }
