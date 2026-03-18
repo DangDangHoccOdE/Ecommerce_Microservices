bb1984cab6901f3eded49d3b4515d2526a5a106e
DangDangHocCode <haidang972004@gmail.com>
2026-03-19 01:42:39 +0700

    Update Notification service: RabbitMQ config and OrderEventConsumer

--- notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
+++ notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
@@ -1,21 +1,36 @@
 package com.ecommerce.notification;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import java.beans.BeanProperty;
+import java.util.function.Consumer;
+
+// import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.notification.payload.OrderCreatedEvent;
+import org.springframework.context.annotation.Bean;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class OrderEventConsumer {
-    @RabbitListener(queues = "${rabbitmq.queue.name}")
-    public void handleOrderEvent(OrderCreatedEvent orderEvent) {
-        System.out.println("Received order event: " + orderEvent);
+    // @RabbitListener(queues = "${rabbitmq.queue.name}")
+    // public void handleOrderEvent(OrderCreatedEvent orderEvent) {
+    // System.out.println("Received order event: " + orderEvent);
 
-        Long orderId = orderEvent.getOrderId();
-        String status = orderEvent.getStatus().toString();
+    // Long orderId = orderEvent.getOrderId();
+    // String status = orderEvent.getStatus().toString();
 
-        System.out.println("Processing order event - Order ID: " + orderId + ", Status: " + status);
+    // System.out.println("Processing order event - Order ID: " + orderId + ",
+    // Status: " + status);
 
-        // update database, send email, etc
+    // // update database, send email, etc
+    // }
+
+    @Bean
+    public Consumer<OrderCreatedEvent> orderCreated() {
+        return event -> {
+            log.info("Received order event: {}", event.getOrderId());
+            log.info("Processing order event - User ID: {}", event.getUserId());
+        };
     }
 }
